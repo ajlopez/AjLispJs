c740e07e016830b62fdf33674b4665ffe7fc4ead
ajlopez <webmaster@ajlopez.com>
2012-05-17 20:20:04 -0300

    closure apply first test

--- test/test-apply.js
+++ test/test-apply.js
@@ -28,3 +28,19 @@ exports['Rest Form Apply'] = function(test) {
 	
 	test.done();
 };
+
+exports['Closure Form Apply wo/Parameters'] = function(test) {
+	var list1 = ajlisp.makeList(ajlisp.environment.define, new ajlisp.Atom("one"), 1);
+	var list2 = ajlisp.makeList(ajlisp.environment.define, new ajlisp.Atom("two"), 2);
+	var list3 = ajlisp.makeList(ajlisp.environment.define, new ajlisp.Atom("three"), 3);
+	var body = ajlisp.makeList(list1, list2, list3);
+	var closure = new ajlisp.Closure(null, ajlisp.environment, body);
+	
+	test.equal(closure.apply(null, ajlisp.environment), 3);
+	test.equal(ajlisp.environment.one, 1);
+	test.equal(ajlisp.environment.two, 2);
+	test.equal(ajlisp.environment.three, 3);
+	
+	test.done();
+};
+
